e4e62098693fe95e71f83915715a7fcb33d3a766
GuyTuval <guytuval@gmail.com>
2020-09-18 17:35:27 +0300

    Extracted queries generation logic to neo4j_queries_generator.py

---
+++ data_access_layer/neo4j_database/neo4j_queries_generator.py
@@ -0,0 +1,80 @@
+from typing import Dict, Iterable, Set
+
+from models.edge_model import EdgeModel
+from models.node_model import NodeModel
+
+
+class Neo4jQueriesGenerator:
+    def __init__(self, edges: Iterable[EdgeModel]):
+        self._edges = edges
+
+    def generate_creation_query(self) -> str:
+        creation_query = f'CREATE '
+        creation_query_variables = set()
+        for edge in self._edges:
+            creation_query_variables.add(self._variable_name(edge))
+            node_queries = []
+            for node in [edge.source_node, edge.destination_node]:  # NOTICE! the list's elements order is important!
+                creation_query_variables.add(self._variable_name(node))
+
+                should_generate_labels_and_properties_query = self._variable_name(node) not in creation_query_variables
+                node_queries.append(self._generate_node_query(node, should_generate_labels_and_properties_query))
+
+            # Concat the source node query and the destination node query using the relationship query
+            creation_query += f'-[{self._variable_name(edge)}:{":".join(edge.labels)}]->'.join(node_queries)
+            creation_query += ', '
+
+        creation_query = creation_query.rstrip().rstrip(',')  # remove whitespaces and extra ','
+        creation_query += f' RETURN {", ".join(creation_query_variables)}'
+        return creation_query
+
+    def _generate_node_query(self, node: NodeModel, should_generate_labels_and_properties_query: bool) -> str:
+        """
+        Generates a query related to the node part. For example, (id_2153351119296:Person:Murderer {id:'3'})
+        :param creation_query_variables: A set of all
+        :param node: The node to generate into a query
+        :return: The node part of the cypher query
+        """
+        creation_query = f'({self._variable_name(node)}'
+        if should_generate_labels_and_properties_query:
+            properties_dict = node.properties
+            properties_dict.update({'id': node.id})
+            creation_query += self._generate_labels_and_properties_query(node.labels, properties_dict)
+        creation_query += ')'
+        return creation_query
+
+    @staticmethod
+    def _generate_labels_and_properties_query(labels: Iterable[str], properties: Dict[str, str]) -> str:
+        labels_and_properties = f':{":".join(labels)}'
+        if len(properties) > 0:
+            labels_and_properties += Neo4jQueriesGenerator._generate_properties_query(properties)
+        return labels_and_properties
+
+    @staticmethod
+    def _generate_properties_query(labels_dict: Dict[str, str]) -> str:
+        labels_query = ' {'
+        for label, value in labels_dict.items():
+            labels_query += f"{label}:\'{value}\', "
+        labels_query = labels_query.rstrip().rstrip(',')  # remove whitespaces and extra ','
+        labels_query += '}'
+        return labels_query
+
+    @staticmethod
+    def _variable_name(obj: any) -> str:
+        return 'id_' + str(id(obj))
+
+    def generate_match_query(self) -> str:
+        match_query = 'MATCH '
+        connections = []
+        match_query_variables = set()
+        for edge in self._edges:
+            connection = f'({self._variable_name(edge.source_node)}{self._generate_labels_and_properties_query(edge.source_node.labels, edge.source_node.properties)})'
+            connection += f'-[{self._variable_name(edge)}:{":".join(edge.labels)}]-'
+            connection += f'({self._variable_name(edge.destination_node)}{self._generate_labels_and_properties_query(edge.destination_node.labels, edge.destination_node.properties)})'
+            connections.append(connection)
+            match_query_variables |= {
+                self._variable_name(edge), self._variable_name(edge.source_node), self._variable_name(edge.destination_node)
+            }
+        match_query += ', '.join(connections)
+        match_query += f' RETURN {", ".join(match_query_variables)}'
+        return match_query
--- test.py
+++ test.py
@@ -1,72 +1,10 @@
 #!/usr/bin/env python3
-from typing import Dict, Iterable
 
 import neo4j
 
+from data_access_layer.neo4j_database.neo4j_queries_generator import Neo4jQueriesGenerator
 from models.edge_model import EdgeModel
 from models.node_model import NodeModel
-
-
-def generate_creation_query(edges: Iterable[EdgeModel]) -> str:
-    creation_query = f'CREATE '
-    creation_query_variables = set()
-    for edge in edges:
-        creation_query_variables.add(variable_name(edge))
-        for node in [edge.source_node, edge.destination_node]:
-            creation_query += f'({variable_name(node)}'
-            if variable_name(node) not in creation_query_variables:
-                creation_query_variables.add(variable_name(node))
-                properties_dict = node.properties
-                properties_dict.update({'id': node.id})
-                creation_query += _generate_labels_and_properties_query(node.labels, properties_dict)
-            creation_query += ')'
-
-            if node.id == edge.source_node.id:
-                creation_query += f'-[{variable_name(edge)}:{":".join(edge.labels)}]->'
-
-        creation_query += ', '
-
-    creation_query = creation_query.rstrip().rstrip(',')  # remove whitespaces and extra ','
-    creation_query += f' RETURN {", ".join(creation_query_variables)}'
-    return creation_query
-
-
-def _generate_labels_and_properties_query(labels: Iterable[str], properties: Dict[str, str]) -> str:
-    labels_and_properties = f':{":".join(labels)}'
-    if len(properties) > 0:
-        labels_and_properties += _generate_properties_query(properties)
-    return labels_and_properties
-
-
-def _generate_properties_query(labels_dict: Dict[str, str]) -> str:
-    labels_query = ' {'
-    for label, value in labels_dict.items():
-        labels_query += f"{label}:\'{value}\', "
-    labels_query = labels_query.rstrip().rstrip(',')  # remove whitespaces and extra ','
-    labels_query += '}'
-    return labels_query
-
-
-def variable_name(obj: any) -> str:
-    return 'id_' + str(id(obj))
-
-
-def generate_match_query(edges: Iterable[EdgeModel]) -> str:
-    match_query = 'MATCH '
-    connections = []
-    match_query_variables = set()
-    for edge in edges:
-        connection = f'({variable_name(edge.source_node)}{_generate_labels_and_properties_query(edge.source_node.labels, edge.source_node.properties)})'
-        connection += f'-[{variable_name(edge)}:{":".join(edge.labels)}]-'
-        connection += f'({variable_name(edge.destination_node)}{_generate_labels_and_properties_query(edge.destination_node.labels, edge.destination_node.properties)})'
-        connections.append(connection)
-        match_query_variables |= {
-            variable_name(edge), variable_name(edge.source_node), variable_name(edge.destination_node)
-        }
-    match_query += ', '.join(connections)
-    match_query += f' RETURN {", ".join(match_query_variables)}'
-    return match_query
-
 
 if __name__ == '__main__':
     node1 = NodeModel(neo4j.data.Node(None, n_id='1', n_labels={'Person'}, properties=dict()))
@@ -77,6 +15,6 @@ if __name__ == '__main__':
     )
     node3 = NodeModel(neo4j.data.Node(None, n_id='3', n_labels={'Person', 'Murderer'}, properties=dict()))
     this_edges = [EdgeModel({'FRIENDS'}, node1, node2), EdgeModel({'ENEMIES'}, node1, node3)]
-    print(generate_creation_query(this_edges))
+    print(Neo4jQueriesGenerator(this_edges).generate_creation_query())
     print('===============MATCH:=================')
-    print(generate_match_query(this_edges))
+    print(Neo4jQueriesGenerator(this_edges).generate_match_query())
