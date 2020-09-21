b88a6cdbe3cf6de8110d14ac8af9a3a40ca6f8a2
GuyTuval <guytuval@gmail.com>
2020-09-21 18:58:44 +0300

    Avoided code duplication.

--- data_access_layer/neo4j_database/neo4j_db_controller.py
+++ data_access_layer/neo4j_database/neo4j_db_controller.py
@@ -24,12 +24,16 @@ class Neo4jDbController(AbstractDbController):
 
     @staticmethod
     def _create_transaction_function(tx, create_query: str) -> List[Union[NodeModel, EdgeModel]]:
-        create_transaction_result = tx.run(create_query)
         try:
-            return Neo4jDbController._generate_nodes_and_edges_list(create_transaction_result)
+            return Neo4jDbController._generic_transaction_function(tx, create_query)
         except ServiceUnavailable as exception:
             logging.error(f"{create_query} raised an error: \n {exception}")
             raise
+
+    @staticmethod
+    def _generic_transaction_function(tx, query: str):
+        transaction_result = tx.run(query)
+        return Neo4jDbController._generate_nodes_and_edges_list(transaction_result)
 
     def match(self, match_query: str) -> List[Union[NodeModel, EdgeModel]]:
         with self._driver.session() as session:
@@ -38,8 +42,9 @@ class Neo4jDbController(AbstractDbController):
 
     @staticmethod
     def _match_transaction_function(tx, match_query: str) -> List[Union[NodeModel, EdgeModel]]:
-        match_transaction_result = tx.run(match_query)
-        return Neo4jDbController._generate_nodes_and_edges_list(match_transaction_result)
+        return Neo4jDbController._generic_transaction_function(tx, match_query)
+        # match_transaction_result = tx.run(match_query)
+        # return Neo4jDbController._generate_nodes_and_edges_list(match_transaction_result)
 
     @staticmethod
     def _generate_nodes_and_edges_list(transaction_result: neo4j.Result) -> List[Union[NodeModel, EdgeModel]]:
@@ -50,5 +55,7 @@ class Neo4jDbController(AbstractDbController):
                 if isinstance(entity, neo4j.data.Node):
                     result.append(NodeModel(entity))
                 elif isinstance(entity, neo4j.data.Relationship):
-                    result.append(EdgeModel({entity.type}, NodeModel(entity.nodes[0]), NodeModel(entity.nodes[1])))
+                    result.append(
+                        EdgeModel.to_edge_model({entity.type}, NodeModel(entity.nodes[0]), NodeModel(entity.nodes[1]))
+                    )
         return result
