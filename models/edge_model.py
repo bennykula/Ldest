08262707bd3412144bd38e4f84cd1e209b54890f
GuyTuval <guytuval@gmail.com>
2020-09-22 17:16:59 +0300

    Made sure there are no duplicates.

--- models/edge_model.py
+++ models/edge_model.py
@@ -13,7 +13,7 @@ class EdgeModel:
     destination_node: NodeModel
 
     def __init__(self, edge_dict: Dict[str, any]):
-        self.labels = list(edge_dict['labels'])
+        self.labels = list(set(edge_dict['labels']))
         source_node_dict = edge_dict['source_node']
         self.source_node = NodeModel(
             neo4j.data.Node(
